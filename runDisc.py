4d369ec0535369e2daaf0ff11bf6aeeee729af12
arumugam666 <rm.arumugam.2000@gmail.com>
2020-03-19 18:19:00 +0800

    limiting execution time to 15 seconds

--- runDisc.py
+++ runDisc.py
@@ -1,12 +1,25 @@
 import sys, os
 import traceback
+import resource 
 from io import StringIO
+  
+# checking time limit exceed 
+def time_exceeded(signo, frame): 
+    print("Time's up !") 
+    raise SystemExit(1) 
+  
+def set_max_runtime(seconds): 
+    # setting up the resource limit 
+    soft, hard = resource.getrlimit(resource.RLIMIT_CPU) 
+    resource.setrlimit(resource.RLIMIT_CPU, (seconds, hard)) 
+    signal.signal(signal.SIGXCPU, time_exceeded) 
 
 def execAndTrackError(inputString):
     result = StringIO()
     sys.stdout = result
     exc_info = None
     try:
+        set_max_runtime(15) 
         exec(inputString)
     except:
         try:
